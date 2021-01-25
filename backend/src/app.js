f7ba9b8bc5f444fb89f2362d8e288138940aa149
Sebastian De Vita <sebassdevita@gmail.com>
2021-01-25 09:59:55 -0300

    Implementadas rutas base del servidor

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,21 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
+//CONFIGURACIONES
+
+app.set('port', process.env.PORT || 4000)
 
 
+//MIDDLEWARES
+
+app.use(cors())
+app.use(express.json())
+
+//RUTAS
+
+app.get('/api/usuarios', (req, res) => res.send('Usuarios routes'))
+app.get('/api/tareas', (req, res) => res.send('Tareas routes'))
 
 module.exports = app
 
--- backend/src/database.js
+++ backend/src/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-console.log(process.env.MONGODB_URI)
+
 const URI = process.env.MONGODB_URI
 ? process.env.MONGODB_URI
 : 'mongodb://localhost/dbtest'
